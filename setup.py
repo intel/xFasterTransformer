5656f2dcbfe4e151e2f0cbcc7cc36613fb24ccb0
Duyi-Wang <duyi.wang@intel.com>
2023-10-19 13:51:31 +0800

    [Dist] Update long description and email. (#9)
    Signed-off-by: Duyi-Wang <duyi.wang@intel.com>

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 import os
 from setuptools import setup, Extension, find_packages
 from setuptools.command.build_ext import build_ext
+from pathlib import Path
+
+# read the contents of your README file
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 
 class CMakeExtension(Extension):
@@ -36,10 +41,12 @@ setup(
     version="1.0.0",
     keywords="LLM",
     description="Boost large language model inference performance on CPU platform.",
-    long_description="Boost large language model inference performance on CPU platform.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     license="Apache 2.0",
     url="https://github.com/intel/xFasterTransformer",
     author="xFasterTransformer",
+    author_email='xft.maintainer@intel.com',
     python_requires=">=3.8",
     package_dir={"": "src"},
     packages=find_packages(where="src", include=["xfastertransformer"]),
@@ -47,7 +54,5 @@ setup(
     platforms="x86_64",
     ext_modules=[CMakeExtension(name="xft")],
     cmdclass={"build_ext": BuildCMakeExt},
-    install_requires=[
-        'torch>=2.0.0'
-    ]
+    install_requires=["torch>=2.0.0, <2.1.0"],
 )
