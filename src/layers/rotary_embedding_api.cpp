a066693fd43888302b312fc54d9bbb604d18b8b9
liuxiaodong <xiaodong2.liu@intel.com>
2023-11-02 02:45:03 -0400

    [API]Rotary Embedding bf16 api for llama

--- src/layers/rotary_embedding_api.cpp
+++ src/layers/rotary_embedding_api.cpp
@@ -1,10 +1,10 @@
 #include "rotary_embedding_api.h"
 
 void xft_rotary_embedding_kernel(
-    const int64_t *__restrict__ position_ids, float *__restrict__ query,
-    float *__restrict__ key, const float *__restrict__ emb_cos,
-    const float *__restrict__ emb_sin, const int dim, const int qStride,
-    const int kStride, const int num_tokens, const int head_num,
+    const int64_t *__restrict__ position_ids, bfloat16_t *__restrict__ query,
+    bfloat16_t *__restrict__ key, const bfloat16_t *__restrict__ emb_cos,
+    const bfloat16_t *__restrict__ emb_sin, const int dim, const int qstride,
+    const int kstride, const int num_tokens, const int head_num,
     const int head_size, const int num_kv_heads = 0) {
   REQUIRES(dim == head_size, "Incorrect shape, rot_dim is not the head size.");
   const int half = (dim + 1) / 2; // inv_freq_size
@@ -14,12 +14,12 @@ void xft_rotary_embedding_kernel(
     int off = head * dim;
 
     for (int row = 0; row < num_tokens; ++row) {
-      float *p1 = query + row * qStride + off;
-      float *p2 = key + row * kStride + off;
+      bfloat16_t *p1 = query + row * qstride + off;
+      bfloat16_t *p2 = key + row * kstride + off;
 
       int pos = position_ids[row];
-      const float *pcos = emb_cos + pos * dim;
-      const float *psin = emb_sin + pos * dim;
+      const bfloat16_t *pcos = emb_cos + pos * dim;
+      const bfloat16_t *psin = emb_sin + pos * dim;
 
 #pragma omp simd
       for (int i = 0; i < half; ++i) {
--- src/layers/rotary_embedding_api.h
+++ src/layers/rotary_embedding_api.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "bfloat16.h"
 #include "compile_util.h"
 #include <cmath>
 #include <cstring>
@@ -6,10 +7,10 @@
 
 void xft_rotary_embedding_kernel(
     const int64_t *__restrict__ position_ids, // [num_tokens]
-    float *__restrict__ query,         // [num_tokens, head_num, head_size]
-    float *__restrict__ key,           // [num_tokens, head_num, head_size]
-    const float *__restrict__ emb_cos, // [max_position, dim]
-    const float *__restrict__ emb_sin, // [max_position, dim]
-    const int dim,                     // rot_dim
-    const int qStride, const int kStride, const int num_tokens,
+    bfloat16_t *__restrict__ query,         // [num_tokens, head_num, head_size]
+    bfloat16_t *__restrict__ key,           // [num_tokens, head_num, head_size]
+    const bfloat16_t *__restrict__ emb_cos, // [max_position, dim]
+    const bfloat16_t *__restrict__ emb_sin, // [max_position, dim]
+    const int dim,                          // rot_dim
+    const int qstride, const int kstride, const int num_tokens,
     const int head_num, const int head_size, const int num_kv_heads);
