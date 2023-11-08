80504e6fd0870314eece0936dd4c256ab0cf4622
xiaodongliu <xiaodong2.liu@intel.com>
2023-11-08 02:16:38 -0500

    rotaryEmbedding API rename 'xftRoataryEmbeddingKernel' to 'rotaryEmbeddingKernel'

--- include/rotary_embedding_api.h
+++ include/rotary_embedding_api.h
@@ -1,19 +1,18 @@
 #pragma once
-#include "compile_util.h"
-#include "dtype.h"
 #include <cmath>
 #include <cstring>
 #include <iostream>
+#include "compile_util.h"
+#include "dtype.h"
 
 namespace xft {
-void xftRotaryEmbeddingKernel(DataType dt,
-                              const int64_t *positionIds, // [num_tokens]
-                              void *query, // [num_tokens, head_num, head_size]
-                              void *key,   // [num_tokens, head_num, head_size]
-                              const void *embCos, // [max_position, dim]
-                              const void *embSin, // [max_position, dim]
-                              const int dim,      // rot_dim
-                              const int qStride, const int kStride,
-                              const int numTokens, const int headNum,
-                              const int headSize, const int numKvHeads);
+void rotaryEmbeddingKernel(DataType dt,
+        const int64_t *positionIds, // [num_tokens]
+        void *query, // [num_tokens, head_num, head_size]
+        void *key, // [num_tokens, head_num, head_size]
+        const void *embCos, // [max_position, dim]
+        const void *embSin, // [max_position, dim]
+        const int dim, // rot_dim
+        const int qStride, const int kStride, const int numTokens, const int headNum, const int headSize,
+        const int numKvHeads);
 }
--- src/layers/rotary_embedding_api.cpp
+++ src/layers/rotary_embedding_api.cpp
@@ -2,93 +2,81 @@
 #include "bfloat16.h"
 namespace xft {
 
-void xftRotaryEmbeddingKernel(const int64_t *positionIds, float *query,
-                              float *key, const float *embCos,
-                              const float *embSin, const int dim,
-                              const int qStride, const int kStride,
-                              const int numTokens, const int headNum,
-                              const int headSize, const int numKvHeads = 0) {
-  REQUIRES(dim == headSize, "Incorrect shape, rot_dim is not the head size.");
-  const int half = (dim + 1) / 2;
+void rotaryEmbeddingKernel(const int64_t *positionIds, float *query, float *key, const float *embCos,
+        const float *embSin, const int dim, const int qStride, const int kStride, const int numTokens,
+        const int headNum, const int headSize, const int numKvHeads = 0) {
+    REQUIRES(dim == headSize, "Incorrect shape, rot_dim is not the head size.");
+    const int half = (dim + 1) / 2;
 
 #pragma omp parallel for
-  for (int head = 0; head < headNum; ++head) {
-    int off = head * dim;
+    for (int head = 0; head < headNum; ++head) {
+        int off = head * dim;
 
-    for (int row = 0; row < numTokens; ++row) {
-      float *p1 = query + row * qStride + off;
-      float *p2 = key + row * kStride + off;
+        for (int row = 0; row < numTokens; ++row) {
+            float *p1 = query + row * qStride + off;
+            float *p2 = key + row * kStride + off;
 
-      int pos = positionIds[row];
-      const float *pcos = embCos + pos * dim;
-      const float *psin = embSin + pos * dim;
+            int pos = positionIds[row];
+            const float *pcos = embCos + pos * dim;
+            const float *psin = embSin + pos * dim;
 
 #pragma omp simd
-      for (int i = 0; i < half; ++i) {
-        auto t1 = p1[i];
-        auto t2 = p2[i];
+            for (int i = 0; i < half; ++i) {
+                auto t1 = p1[i];
+                auto t2 = p2[i];
 
-        p1[i] = p1[i] * pcos[i] - p1[i + half] * psin[i];
-        p2[i] = p2[i] * pcos[i] - p2[i + half] * psin[i];
+                p1[i] = p1[i] * pcos[i] - p1[i + half] * psin[i];
+                p2[i] = p2[i] * pcos[i] - p2[i + half] * psin[i];
 
-        p1[i + half] = p1[i + half] * pcos[i + half] + t1 * psin[i + half];
-        p2[i + half] = p2[i + half] * pcos[i + half] + t2 * psin[i + half];
-      }
+                p1[i + half] = p1[i + half] * pcos[i + half] + t1 * psin[i + half];
+                p2[i + half] = p2[i + half] * pcos[i + half] + t2 * psin[i + half];
+            }
+        }
     }
-  }
 }
 
-void xftRotaryEmbeddingKernel(const int64_t *position_ids, bfloat16_t *query,
-                              bfloat16_t *key, const bfloat16_t *embCos,
-                              const bfloat16_t *embSin, const int dim,
-                              const int qStride, const int kStride,
-                              const int numTokens, const int headNum,
-                              const int headSize, const int numKvHeads = 0) {
-  REQUIRES(dim == headSize, "Incorrect shape, rot_dim is not the head size.");
-  const int half = (dim + 1) / 2;
+void rotaryEmbeddingKernel(const int64_t *position_ids, bfloat16_t *query, bfloat16_t *key, const bfloat16_t *embCos,
+        const bfloat16_t *embSin, const int dim, const int qStride, const int kStride, const int numTokens,
+        const int headNum, const int headSize, const int numKvHeads = 0) {
+    REQUIRES(dim == headSize, "Incorrect shape, rot_dim is not the head size.");
+    const int half = (dim + 1) / 2;
 
 #pragma omp parallel for
-  for (int head = 0; head < headNum; ++head) {
-    int off = head * dim;
+    for (int head = 0; head < headNum; ++head) {
+        int off = head * dim;
 
-    for (int row = 0; row < numTokens; ++row) {
-      bfloat16_t *p1 = query + row * qStride + off;
-      bfloat16_t *p2 = key + row * kStride + off;
+        for (int row = 0; row < numTokens; ++row) {
+            bfloat16_t *p1 = query + row * qStride + off;
+            bfloat16_t *p2 = key + row * kStride + off;
 
-      int pos = position_ids[row];
-      const bfloat16_t *pcos = embCos + pos * dim;
-      const bfloat16_t *psin = embSin + pos * dim;
+            int pos = position_ids[row];
+            const bfloat16_t *pcos = embCos + pos * dim;
+            const bfloat16_t *psin = embSin + pos * dim;
 
 #pragma omp simd
-      for (int i = 0; i < half; ++i) {
-        auto t1 = p1[i];
-        auto t2 = p2[i];
+            for (int i = 0; i < half; ++i) {
+                auto t1 = p1[i];
+                auto t2 = p2[i];
 
-        p1[i] = p1[i] * pcos[i] - p1[i + half] * psin[i];
-        p2[i] = p2[i] * pcos[i] - p2[i + half] * psin[i];
+                p1[i] = p1[i] * pcos[i] - p1[i + half] * psin[i];
+                p2[i] = p2[i] * pcos[i] - p2[i + half] * psin[i];
 
-        p1[i + half] = p1[i + half] * pcos[i + half] + t1 * psin[i + half];
-        p2[i + half] = p2[i + half] * pcos[i + half] + t2 * psin[i + half];
-      }
+                p1[i + half] = p1[i + half] * pcos[i + half] + t1 * psin[i + half];
+                p2[i + half] = p2[i + half] * pcos[i + half] + t2 * psin[i + half];
+            }
+        }
     }
-  }
 }
 
-void xftRotaryEmbeddingKernel(DataType dt, const int64_t *positionIds,
-                              void *query, void *key, const void *embCos,
-                              const void *embSin, const int dim,
-                              const int qStride, const int kStride,
-                              const int numTokens, const int headNum,
-                              const int headSize, const int numKvHeads) {
-  if (dt == DataType::bf16) {
-    xftRotaryEmbeddingKernel(positionIds, (bfloat16_t *)query,
-                             (bfloat16_t *)key, (bfloat16_t *)embCos,
-                             (bfloat16_t *)embSin, dim, qStride, kStride,
-                             numTokens, headNum, headSize);
-  } else if (dt == DataType::fp16) {
-    xftRotaryEmbeddingKernel(positionIds, (float *)query, (float *)key,
-                             (float *)embCos, (float *)embSin, dim, qStride,
-                             kStride, numTokens, headNum, headSize);
-  }
+void rotaryEmbeddingKernel(DataType dt, const int64_t *positionIds, void *query, void *key, const void *embCos,
+        const void *embSin, const int dim, const int qStride, const int kStride, const int numTokens, const int headNum,
+        const int headSize, const int numKvHeads) {
+    if (dt == DataType::bf16) {
+        rotaryEmbeddingKernel(positionIds, (bfloat16_t *)query, (bfloat16_t *)key, (bfloat16_t *)embCos,
+                (bfloat16_t *)embSin, dim, qStride, kStride, numTokens, headNum, headSize);
+    } else if (dt == DataType::fp16) {
+        rotaryEmbeddingKernel(positionIds, (float *)query, (float *)key, (float *)embCos, (float *)embSin, dim, qStride,
+                kStride, numTokens, headNum, headSize);
+    }
 }
 } // namespace xft
