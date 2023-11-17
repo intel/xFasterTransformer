7e6ce96dd628dbc9243a68f6b9c268e21ca41e8f
changqi1 <changqing.li@intel.com>
2023-11-17 09:53:12 +0800

    [xDNN] Update xDNN version 1.2.1

--- cmake/xdnn.cmake
+++ cmake/xdnn.cmake
@@ -26,8 +26,8 @@ include(ExternalProject)
 
 # cmake-format: off
 ExternalProject_Add(xdnn_lib
-  URL               https://github.com/intel/xFasterTransformer/releases/download/IntrinsicGemm/xdnn_v1.2.tar.gz
-  URL_HASH          MD5=4b395eed4ee08476a7634709b2e12ee2
+  URL               https://github.com/intel/xFasterTransformer/releases/download/IntrinsicGemm/xdnn_v1.2.1.tar.gz
+  URL_HASH          MD5=b55b5d58c92339aa088dcc6e1df6ede2
   TIMEOUT           60
   SOURCE_DIR        ${CMAKE_SOURCE_DIR}/3rdparty/xdnn
   CONFIGURE_COMMAND ""
