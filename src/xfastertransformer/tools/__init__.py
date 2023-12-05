027ddb4fdd94e883ebb079bcca3198df98bc95e3
SakuraYM <meng.yu@intel.com>
2023-12-05 14:26:09 +0800

    Add ChatGLM3 support (#110)

--- src/xfastertransformer/tools/__init__.py
+++ src/xfastertransformer/tools/__init__.py
@@ -15,6 +15,6 @@
 
 from .llama_convert import LlamaConvert
 from .chatglm_convert import ChatGLMConvert
-from .chatglm2_convert import ChatGLM2Convert
+from .chatglm23_convert import ChatGLM23Convert
 from .opt_convert import OPTConvert
 from .baichuan_convert import BaichuanConvert
