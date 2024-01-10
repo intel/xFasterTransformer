4f69d9479821b4ae4566cc5b175f0d65d1b692b6
Meng,Chen <chen.meng@intel.com>
2024-01-10 16:10:23 +0800

    [Model][SecLLM] Add SecLLM(YaRN-Llama) model support (#172)

---
+++ src/models/yarn_llama.h
@@ -0,0 +1,42 @@
+// Copyright (c) 2023 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ============================================================================
+#pragma once
+
+#include "common_decoder.h"
+#include "mlp_llama.h"
+#include "rms_norm.h"
+#include "attn_rope_scaling.h"
+#include "token_embedding.h"
+#include "yarn_scaled_rotary_embedding.h"
+
+template <typename WeiT>
+class YaRNLlama
+    : public CommonDecoder<RopeScalingAttention<WeiT, LlamaYaRNScaledRotaryEmbedding, RmsNorm>, LlamaMLP<WeiT>, float> {
+public:
+    YaRNLlama(const std::string &modelPath);
+    ~YaRNLlama();
+
+    void prepareAttnMask(int *ids, int step);
+    void embeddingForward(int *ids, float *output, int batchSize, int seqLen);
+    void lastLayerNormForward(float *input, float *output, int rows);
+
+private:
+    void setEmbeddingWeights(const std::string &modelPath);
+    void setFinalLnWeight(const std::string &modelPath);
+
+private:
+    TokenEmbedding<float16_t> *embedding;
+    RmsNorm finalLN;
+};
--- tools/yarn_llama_convert.py
+++ tools/yarn_llama_convert.py
@@ -1,5 +1,20 @@
+# Copyright (c) 2023 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
 """
-Convert huggingface Llama/Llama2 model. Use https://huggingface.co/meta-llama
+Convert huggingface YaRN-Llama(SecLLM) model.
 """
 
 import argparse
@@ -13,7 +28,7 @@ import torch
 from datetime import datetime
 from pathlib import Path
 from tqdm import tqdm
-from transformers import LlamaForCausalLM, LlamaTokenizer
+from transformers import AutoModelForCausalLM
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(dir_path, "../../../.."))
@@ -80,6 +95,23 @@ def split_and_convert_process(
             val = np.concatenate((q_split_vals[j], k_split_vals[j], v_split_vals[j]), axis=-1)
             save_val(val, key, i * factor + j)
 
+    elif "attention.query_key_value.bias" in key:
+        hidden_dim = val.shape[0]
+        head_size = int(hidden_dim / (int(num_attention_heads) + int(num_key_value_heads) * 2))
+        qcol = int(num_attention_heads) * head_size
+        kcol = int(num_key_value_heads) * head_size
+        vcol = int(num_key_value_heads) * head_size
+        qkv = np.split(val, [qcol, qcol + kcol])
+        q = qkv[0]
+        k = qkv[1]
+        v = qkv[2]
+        q_split_vals = np.split(q, factor, axis=-1)
+        k_split_vals = np.split(k, factor, axis=-1)
+        v_split_vals = np.split(v, factor, axis=-1)
+        for j in range(factor):
+            val = np.concatenate((q_split_vals[j], k_split_vals[j], v_split_vals[j]), axis=-1)
+            save_val(val, key, i * factor + j)
+
     elif "attention.dense.weight" in key:
         split_vals = np.split(val, factor, axis=0)
         for j in range(factor):
@@ -97,13 +129,7 @@ def split_and_convert(args):
         os.makedirs(saved_dir)
 
     # load the model
-    model = LlamaForCausalLM.from_pretrained(
-        args.in_file,
-        load_in_8bit=False,
-        torch_dtype=torch.float16,
-        low_cpu_mem_usage=True,
-        device_map="auto",
-    )
+    model = AutoModelForCausalLM.from_pretrained(args.in_file, device_map="auto", trust_remote_code=True)
 
     hf_config = vars(model.config)
 
@@ -111,28 +137,38 @@ def split_and_convert(args):
 
     # save parameters to config file
     config = configparser.ConfigParser()
-    config["llama"] = {}
+    config["yarn_llama"] = {}
     has_post_decoder_layernorm = True
     try:
-        config["llama"]["model_name"] = "llama" if hf_config["_name_or_path"] == "" else hf_config["_name_or_path"]
-        num_attention_heads = config["llama"]["head_num"] = str(hf_config["num_attention_heads"])
-        num_key_value_heads = config["llama"]["kv_head_num"] = str(
+        config["yarn_llama"]["model_name"] = "yarn_llama" if hf_config["_name_or_path"] == "" else hf_config["_name_or_path"]
+        num_attention_heads = config["yarn_llama"]["head_num"] = str(hf_config["num_attention_heads"])
+        num_key_value_heads = config["yarn_llama"]["kv_head_num"] = str(
             hf_config.get("num_key_value_heads", num_attention_heads)
         )
 
         hidden_size = hf_config["hidden_size"]
-        config["llama"]["size_per_head"] = str(hidden_size // hf_config["num_attention_heads"])
-        config["llama"]["inter_size"] = str(hf_config["intermediate_size"])
-        config["llama"]["max_pos_seq_len"] = str(hf_config["max_position_embeddings"])
-        config["llama"]["num_layer"] = str(hf_config["num_hidden_layers"])
-        config["llama"]["layernorm_eps"] = str(hf_config.get("rms_norm_eps", 1e-6))
-        config["llama"]["layernorm_type"] = "pre_layernorm"
-        config["llama"]["activation_type"] = "silu"
-        config["llama"]["has_post_decoder_layernorm"] = "1" if has_post_decoder_layernorm else "0"
-        config["llama"]["vocab_size"] = str(hf_config["vocab_size"])
-        config["llama"]["start_id"] = str(hf_config["bos_token_id"])
-        config["llama"]["end_id"] = str(hf_config["eos_token_id"])
-        config["llama"]["weight_data_type"] = args.weight_data_type
+        config["yarn_llama"]["size_per_head"] = str(hidden_size // hf_config["num_attention_heads"])
+        config["yarn_llama"]["inter_size"] = str(hf_config["intermediate_size"])
+        config["yarn_llama"]["max_pos_seq_len"] = str(hf_config["max_position_embeddings"])
+        config["yarn_llama"]["num_layer"] = str(hf_config["num_hidden_layers"])
+
+        rope_scaling_config = hf_config.get("rope_scaling", {})
+        config["yarn_llama"]["rope_scaling_type"] = str(rope_scaling_config.get("type", ""))
+        config["yarn_llama"]["rope_scaling_factor"] = str(rope_scaling_config.get("factor", 1))
+        config["yarn_llama"]["rope_scaling_original_max_position_embeddings"] = str(
+            rope_scaling_config.get("original_max_position_embeddings", 2048)
+        )
+        config["yarn_llama"]["rope_scaling_finetuned"] = str(rope_scaling_config.get("finetuned", "false"))
+        config["yarn_llama"]["rope_theta"] = str(hf_config.get("rope_theta", 10000))
+
+        config["yarn_llama"]["layernorm_eps"] = str(hf_config.get("rms_norm_eps", 1e-6))
+        config["yarn_llama"]["layernorm_type"] = "pre_layernorm"
+        config["yarn_llama"]["activation_type"] = "silu"
+        config["yarn_llama"]["has_post_decoder_layernorm"] = "1" if has_post_decoder_layernorm else "0"
+        config["yarn_llama"]["vocab_size"] = str(hf_config["vocab_size"])
+        config["yarn_llama"]["start_id"] = str(hf_config["bos_token_id"])
+        config["yarn_llama"]["end_id"] = str(hf_config["eos_token_id"])
+        config["yarn_llama"]["weight_data_type"] = args.weight_data_type
         with open(os.path.join(saved_dir, "config.ini"), "w") as configfile:
             config.write(configfile)
     except Exception as e:
@@ -144,7 +180,9 @@ def split_and_convert(args):
     hf_model_name_pattern = [
         "input_layernorm.weight",
         "attention.query_key_value.weight",
+        "attention.query_key_value.bias",
         "self_attn.o_proj.weight",
+        "self_attn.o_proj.bias",
         "post_attention_layernorm.weight",
         "mlp.gate_proj.weight",
         "mlp.up_proj.weight",
@@ -154,7 +192,9 @@ def split_and_convert(args):
     ft_model_name_pattern = [
         "input_layernorm.weight",
         "attention.query_key_value.weight",
+        "attention.query_key_value.bias",
         "attention.dense.weight",
+        "attention.dense.bias",
         "post_attention_layernorm.weight",
         "mlp.gate_proj.weight",
         "mlp.up_proj.weight",
@@ -173,8 +213,15 @@ def split_and_convert(args):
                 (param.permute(1, 0), state_dict[k_name].permute(1, 0), state_dict[v_name].permute(1, 0)), dim=1
             )
             model_named_parameters[name.replace("self_attn.q_proj.weight", "attention.query_key_value.weight")] = qkv
+        elif "self_attn.q_proj.bias" in name:
+            k_name = name.replace("q_proj", "k_proj")
+            v_name = name.replace("q_proj", "v_proj")
+            qkv_b = torch.cat((param, state_dict[k_name], state_dict[v_name]), dim=0)
+            model_named_parameters[name.replace("self_attn.q_proj.bias", "attention.query_key_value.bias")] = qkv_b
         # for merged weights, skip
         elif "self_attn.k_proj.weight" in name or "self_attn.v_proj.weight" in name:
+            continue
+        elif "self_attn.k_proj.bias" in name or "self_attn.v_proj.bias" in name:
             continue
         elif "embed" in name:
             model_named_parameters[name] = param
@@ -191,9 +238,6 @@ def split_and_convert(args):
             param.detach().cpu().numpy().astype(np_weight_data_type).tofile(
                 os.path.join(saved_dir, "model.final_layernorm.weight.bin")
             )
-        # elif name == 'model.final_layernorm.bias':
-        #     param.detach().cpu().numpy().astype(np_weight_data_type).tofile(
-        #         os.path.join(saved_dir, "model.final_layernorm.bias.bin"))
         elif name == "lm_head.weight":
             param.detach().cpu().numpy().astype(np_weight_data_type).tofile(
                 os.path.join(saved_dir, "model.lm_head.weight.bin")
@@ -238,6 +282,7 @@ if __name__ == "__main__":
     for key in vars(args):
         print(f"{key}: {vars(args)[key]}")
     print("========================================")
+    sys.path.append(args.in_file)
 
     start_time = datetime.now()
     split_and_convert(args)
