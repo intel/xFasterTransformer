c274d0fb84a554a00f2f0e45e6d4615a9116bea7
changqi1 <changqing.li@intel.com>
2024-01-24 16:45:59 +0000

    Build w/ oneDNN GPU library.

--- cmake/onednn.cmake
+++ cmake/onednn.cmake
@@ -27,10 +27,10 @@ include(ExternalProject)
 # cmake-format: off
 ExternalProject_Add(onednn
   GIT_REPOSITORY    https://github.com/oneapi-src/oneDNN.git
-  GIT_TAG           v3.2
+  GIT_TAG           v3.3.3
   SOURCE_DIR        ${CMAKE_SOURCE_DIR}/3rdparty/onednn
   BINARY_DIR        ${CMAKE_SOURCE_DIR}/3rdparty/onednn
-  CONFIGURE_COMMAND ${CMAKE_COMMAND} -E make_directory "build" && ${CMAKE_COMMAND} -E chdir "build" ${CMAKE_COMMAND} -DONEDNN_LIBRARY_TYPE=STATIC -DONEDNN_BUILD_TESTS=OFF -DONEDNN_BUILD_EXAMPLES=OFF ..
+  CONFIGURE_COMMAND ${CMAKE_COMMAND} -E make_directory "build" && ${CMAKE_COMMAND} -E chdir "build" ${CMAKE_COMMAND} -DONEDNN_GPU_RUNTIME=SYCL -DONEDNN_LIBRARY_TYPE=STATIC -DONEDNN_BUILD_TESTS=OFF -DONEDNN_BUILD_EXAMPLES=OFF ..
   BUILD_COMMAND     ${CMAKE_COMMAND} -E chdir "build" make -j all
   INSTALL_COMMAND   ""
   TEST_COMMAND      ""
