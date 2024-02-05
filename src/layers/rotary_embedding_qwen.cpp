384beae4c20a54716492846dd867b7d229db7536
a3213105 <sheng.gui@intel.com>
2024-02-05 14:43:54 +0800

    Add bf16 implementation of Qwne logn

--- src/layers/rotary_embedding_qwen.cpp
+++ src/layers/rotary_embedding_qwen.cpp
@@ -253,6 +253,7 @@ void QwenRotaryEmbedding::forward(
     const int qHeads = qkShape[2];
     const int kHeads = qkShape[4];
     const int maxSeqLength = qkShape[5];
+    const int pastKeyLength = qkShape[6];
     const int heads = std::max(qHeads, kHeads);
     const int half = this->inv_freq_size;
 
@@ -276,46 +277,103 @@ void QwenRotaryEmbedding::forward(
         cur_emb_sin = std::get<1>(value);
     }
 
+    if (seqLen + pastKeyLength > maxSeqLength && logn != nullptr) {
+        float *q_scale = logn + pastKeyLength;
 #pragma omp parallel for collapse(3)
-    for (int head = 0; head < heads; ++head) {
-        for (int bs = 0; bs < batchSize; ++bs) {
-            for (int seq = 0; seq < seqLen; ++seq) {
-                int pos = positionIds[seq];
-                float *pcos = cur_emb_cos + pos * dim;
-                float *psin = cur_emb_sin + pos * dim;
+        for (int head = 0; head < heads; ++head) {
+            for (int bs = 0; bs < batchSize; ++bs) {
+                for (int seq = 0; seq < seqLen; ++seq) {
+                    int pos = positionIds[seq];
+                    float *pcos = cur_emb_cos + pos * dim;
+                    float *psin = cur_emb_sin + pos * dim;
 
-                bfloat16_t *q = query + bs * seqLen * qStride + seq * qStride + head * dim;
-                bfloat16_t *k = key + bs * seqLen * kStride + seq * kStride + head * dim;
+                    bfloat16_t *q = query + bs * seqLen * qStride + seq * qStride + head * dim;
+                    bfloat16_t *k = key + bs * seqLen * kStride + seq * kStride + head * dim;
 
-                // Process chunks of 16 elements at a time
-                for (int i = 0; i < half; i += 16) {
-                    int remain = half - i;
-                    __mmask16 mask = (remain >= 16 ? 0xffff : (1 << remain) - 1);
+                    __m512 pScale = _mm512_set1_ps(q_scale[seq]);
 
-                    __m512 pCosVec = _mm512_maskz_loadu_ps(mask, &pcos[i]);
-                    __m512 pCosHalfVec = _mm512_maskz_loadu_ps(mask, &pcos[i + half]);
-                    __m512 pSinVec = _mm512_maskz_loadu_ps(mask, &psin[i]);
-                    __m512 pSinHalfVec = _mm512_maskz_loadu_ps(mask, &psin[i + half]);
+                    // Process chunks of 16 elements at a time
+                    for (int i = 0; i < half; i += 16) {
+                        int remain = half - i;
+                        __mmask16 mask = (remain >= 16 ? 0xffff : (1 << remain) - 1);
 
-                    // Compute something like:
-                    // q[i] = q[i] * pcos[i] - q[i + half] * psin[i];
-                    // q[i + half] = q[i + half] * pcos[i + half] + q[i] * psin[i + half];
-                    if (head < qHeads) {
-                        __m512 qVec = bfloat16_t::cvt_bf16_to_fp32(_mm256_maskz_loadu_epi16(mask, &q[i]));
-                        __m512 qHalfVec = bfloat16_t::cvt_bf16_to_fp32(_mm256_maskz_loadu_epi16(mask, &q[i + half]));
-                        __m512 qNew = _mm512_fmsub_ps(qVec, pCosVec, _mm512_mul_ps(qHalfVec, pSinVec));
-                        __m512 qHalfNew = _mm512_fmadd_ps(qHalfVec, pCosHalfVec, _mm512_mul_ps(qVec, pSinHalfVec));
-                        _mm256_mask_storeu_epi16(&q[i], mask, bfloat16_t::cvt_fp32_to_bf16(qNew));
-                        _mm256_mask_storeu_epi16(&q[i + half], mask, bfloat16_t::cvt_fp32_to_bf16(qHalfNew));
+                        __m512 pCosVec = _mm512_maskz_loadu_ps(mask, &pcos[i]);
+                        __m512 pCosHalfVec = _mm512_maskz_loadu_ps(mask, &pcos[i + half]);
+                        __m512 pSinVec = _mm512_maskz_loadu_ps(mask, &psin[i]);
+                        __m512 pSinHalfVec = _mm512_maskz_loadu_ps(mask, &psin[i + half]);
+
+                        // Compute something like:
+                        // q[i] = ( q[i] * pcos[i] - q[i + half] * psin[i] )*logN;
+                        // q[i + half] = ( q[i + half] * pcos[i + half] + q[i] * psin[i + half] ) * logN;
+                        if (head < qHeads) {
+                            __m512 qVec = bfloat16_t::cvt_bf16_to_fp32(_mm256_maskz_loadu_epi16(mask, &q[i]));
+                            __m512 qHalfVec
+                                    = bfloat16_t::cvt_bf16_to_fp32(_mm256_maskz_loadu_epi16(mask, &q[i + half]));
+                            __m512 qNew = _mm512_mul_ps(
+                                    _mm512_fmsub_ps(qVec, pCosVec, _mm512_mul_ps(qHalfVec, pSinVec)), pScale);
+                            __m512 qHalfNew = _mm512_mul_ps(
+                                    _mm512_fmadd_ps(qHalfVec, pCosHalfVec, _mm512_mul_ps(qVec, pSinHalfVec)), pScale);
+                            _mm256_mask_storeu_epi16(&q[i], mask, bfloat16_t::cvt_fp32_to_bf16(qNew));
+                            _mm256_mask_storeu_epi16(&q[i + half], mask, bfloat16_t::cvt_fp32_to_bf16(qHalfNew));
+                        }
+
+                        if (head < kHeads) {
+                            __m512 kVec = bfloat16_t::cvt_bf16_to_fp32(_mm256_maskz_loadu_epi16(mask, &k[i]));
+                            __m512 kHalfVec
+                                    = bfloat16_t::cvt_bf16_to_fp32(_mm256_maskz_loadu_epi16(mask, &k[i + half]));
+                            __m512 kNew = _mm512_fmsub_ps(kVec, pCosVec, _mm512_mul_ps(kHalfVec, pSinVec));
+                            __m512 kHalfNew = _mm512_fmadd_ps(kHalfVec, pCosHalfVec, _mm512_mul_ps(kVec, pSinHalfVec));
+                            _mm256_mask_storeu_epi16(&k[i], mask, bfloat16_t::cvt_fp32_to_bf16(kNew));
+                            _mm256_mask_storeu_epi16(&k[i + half], mask, bfloat16_t::cvt_fp32_to_bf16(kHalfNew));
+                        }
                     }
+                }
+            }
+        }
+    } else {
+#pragma omp parallel for collapse(3)
+        for (int head = 0; head < heads; ++head) {
+            for (int bs = 0; bs < batchSize; ++bs) {
+                for (int seq = 0; seq < seqLen; ++seq) {
+                    int pos = positionIds[seq];
+                    float *pcos = cur_emb_cos + pos * dim;
+                    float *psin = cur_emb_sin + pos * dim;
 
-                    if (head < kHeads) {
-                        __m512 kVec = bfloat16_t::cvt_bf16_to_fp32(_mm256_maskz_loadu_epi16(mask, &k[i]));
-                        __m512 kHalfVec = bfloat16_t::cvt_bf16_to_fp32(_mm256_maskz_loadu_epi16(mask, &k[i + half]));
-                        __m512 kNew = _mm512_fmsub_ps(kVec, pCosVec, _mm512_mul_ps(kHalfVec, pSinVec));
-                        __m512 kHalfNew = _mm512_fmadd_ps(kHalfVec, pCosHalfVec, _mm512_mul_ps(kVec, pSinHalfVec));
-                        _mm256_mask_storeu_epi16(&k[i], mask, bfloat16_t::cvt_fp32_to_bf16(kNew));
-                        _mm256_mask_storeu_epi16(&k[i + half], mask, bfloat16_t::cvt_fp32_to_bf16(kHalfNew));
+                    bfloat16_t *q = query + bs * seqLen * qStride + seq * qStride + head * dim;
+                    bfloat16_t *k = key + bs * seqLen * kStride + seq * kStride + head * dim;
+
+                    // Process chunks of 16 elements at a time
+                    for (int i = 0; i < half; i += 16) {
+                        int remain = half - i;
+                        __mmask16 mask = (remain >= 16 ? 0xffff : (1 << remain) - 1);
+
+                        __m512 pCosVec = _mm512_maskz_loadu_ps(mask, &pcos[i]);
+                        __m512 pCosHalfVec = _mm512_maskz_loadu_ps(mask, &pcos[i + half]);
+                        __m512 pSinVec = _mm512_maskz_loadu_ps(mask, &psin[i]);
+                        __m512 pSinHalfVec = _mm512_maskz_loadu_ps(mask, &psin[i + half]);
+
+                        // Compute something like:
+                        // q[i] = q[i] * pcos[i] - q[i + half] * psin[i];
+                        // q[i + half] = q[i + half] * pcos[i + half] + q[i] * psin[i + half];
+                        if (head < qHeads) {
+                            __m512 qVec = bfloat16_t::cvt_bf16_to_fp32(_mm256_maskz_loadu_epi16(mask, &q[i]));
+                            __m512 qHalfVec
+                                    = bfloat16_t::cvt_bf16_to_fp32(_mm256_maskz_loadu_epi16(mask, &q[i + half]));
+                            __m512 qNew = _mm512_fmsub_ps(qVec, pCosVec, _mm512_mul_ps(qHalfVec, pSinVec));
+                            __m512 qHalfNew = _mm512_fmadd_ps(qHalfVec, pCosHalfVec, _mm512_mul_ps(qVec, pSinHalfVec));
+                            _mm256_mask_storeu_epi16(&q[i], mask, bfloat16_t::cvt_fp32_to_bf16(qNew));
+                            _mm256_mask_storeu_epi16(&q[i + half], mask, bfloat16_t::cvt_fp32_to_bf16(qHalfNew));
+                        }
+
+                        if (head < kHeads) {
+                            __m512 kVec = bfloat16_t::cvt_bf16_to_fp32(_mm256_maskz_loadu_epi16(mask, &k[i]));
+                            __m512 kHalfVec
+                                    = bfloat16_t::cvt_bf16_to_fp32(_mm256_maskz_loadu_epi16(mask, &k[i + half]));
+                            __m512 kNew = _mm512_fmsub_ps(kVec, pCosVec, _mm512_mul_ps(kHalfVec, pSinVec));
+                            __m512 kHalfNew = _mm512_fmadd_ps(kHalfVec, pCosHalfVec, _mm512_mul_ps(kVec, pSinHalfVec));
+                            _mm256_mask_storeu_epi16(&k[i], mask, bfloat16_t::cvt_fp32_to_bf16(kNew));
+                            _mm256_mask_storeu_epi16(&k[i + half], mask, bfloat16_t::cvt_fp32_to_bf16(kHalfNew));
+                        }
                     }
                 }
             }
