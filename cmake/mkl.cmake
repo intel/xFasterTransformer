4c2ccceeba3074c70da77c6999c4c1a0125da6cc
Duyi-Wang <duyi.wang@intel.com>
2024-02-22 14:52:09 +0800

    [CMake] Remvoe force reinstall for mkl dependencies. (#237)

--- cmake/mkl.cmake
+++ cmake/mkl.cmake
@@ -21,11 +21,12 @@ if(${CMAKE_VERSION} VERSION_GREATER_EQUAL "3.24.0")
 endif()
 
 find_package (Python COMPONENTS Interpreter Development)
-execute_process(COMMAND ${Python_EXECUTABLE} -m pip install --force-reinstall --prefix=${CMAKE_SOURCE_DIR}/3rdparty/mkl
+execute_process(COMMAND ${Python_EXECUTABLE} -m pip install --prefix=${CMAKE_SOURCE_DIR}/3rdparty/mkl
                         mkl-static==2024.0.0 mkl-include==2024.0.0
                 RESULT_VARIABLE EXIT_CODE
                 )
 
 if(NOT ${EXIT_CODE} EQUAL 0)
         message(STATUS "Dependency MKL installation failed. Please check the logs for more information.")
+        message(STATUS "If `mkl-static` and `mkl-include` has already been installed with pip, please uninstall and try again.")
 endif()
