d2784c4bc1495b22bd47376627bcb70184322f57
xiangzez <xiangze.zhang@intel.com>
2024-02-29 14:11:58 +0800

    Fix Opt issue (#251)
    Load beta weights

--- src/models/opt_decoder.cpp
+++ src/models/opt_decoder.cpp
@@ -64,7 +64,8 @@ void OptDecoder<WeiT>::setEmbeddingWeights(const std::string &modelPath) {
 
 template <typename WeiT>
 void OptDecoder<WeiT>::setFinalLnWeight(const std::string &modelPath) {
-    finalLN.setWeight(modelPath + "/model.final_layernorm.weight.bin", "", embedding->getHiddenSize());
+    finalLN.setWeight(modelPath + "/model.final_layernorm.weight.bin", modelPath + "/model.final_layernorm.bias.bin",
+            embedding->getHiddenSize());
 }
 
 template <typename WeiT>
