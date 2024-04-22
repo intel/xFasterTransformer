399ce889c35013afcc439df01b38b8f6bad8cada
pujiang <pujiang.he@intel.com>
2024-04-21 21:53:34 -0400

    add isPrefill and inputTokens

--- src/common/batch_info.h
+++ src/common/batch_info.h
@@ -24,65 +24,78 @@ class BatchInfo {
 
 public:
     // Constructor for first token
-    BatchInfo(size_t *inputTokens, size_t batchSize) : batchSize(batchSize), allGenPhase(false) {
-        endTokenIdxs.resize(batchSize);
-        pastTokens.resize(batchSize);
+    BatchInfo(size_t *inputTokens, size_t batchSize) : batchSize(batchSize), allInputOne(false), bePrefill(true) {
+        this->inputTokens.resize(batchSize);
+        this->endTokenIdxs.resize(batchSize);
+        this->pastTokens.resize(batchSize);
 
         size_t endIdx = 0;
         for (size_t i = 0; i < batchSize; ++i) {
             endIdx += inputTokens[i];
-            endTokenIdxs[i] = endIdx;
+            this->inputTokens[i] = inputTokens[i];
+            this->endTokenIdxs[i] = endIdx;
         }
 
-        std::fill(pastTokens.begin(), pastTokens.end(), 0);
+        std::fill(this->pastTokens.begin(), this->pastTokens.end(), 0);
     }
 
     // Constructor for next tokens
-    BatchInfo(size_t *inputTokens, size_t *pastSeqs, size_t batchSize) : batchSize(batchSize) {
-        endTokenIdxs.resize(batchSize);
-        pastTokens.resize(batchSize);
+    BatchInfo(size_t *inputTokens, size_t *pastSeqLens, size_t batchSize) : batchSize(batchSize), bePrefill(false) {
+        this->inputTokens.resize(batchSize);
+        this->endTokenIdxs.resize(batchSize);
+        this->pastTokens.resize(batchSize);
 
         if (inputTokens == nullptr) {
-            std::iota(endTokenIdxs.begin(), endTokenIdxs.end(), 1);
-            allGenPhase = true;
+            std::fill(this->inputTokens.begin(), this->inputTokens.end(), 1);
+            std::iota(this->endTokenIdxs.begin(), this->endTokenIdxs.end(), 1);
+            this->allInputOne = true;
         } else {
-            bool allGenPhase = true;
+            this->allInputOne = true;
             size_t endIdx = 0;
             for (size_t i = 0; i < batchSize; ++i) {
                 endIdx += inputTokens[i];
-                endTokenIdxs[i] = endIdx;
-                if (inputTokens[i] != 1) { allGenPhase = false; }
+                this->inputTokens[i] = inputTokens[i];
+                this->endTokenIdxs[i] = endIdx;
+                if (inputTokens[i] != 1) { this->allInputOne = false; }
             }
         }
 
         // Assign pastSeqs to pastTokens
         for (size_t i = 0; i < batchSize; ++i) {
-            pastTokens[i] = pastSeqs[i];
+            this->pastTokens[i] = pastSeqLens[i];
         }
     }
 
     // Get the batch size
     size_t getBatchSize() const { return batchSize; }
 
+    // Get the input tokens for each sample inside the batch
+    const size_t *getInputTokens() const {
+        return inputTokens.data();
+    }
+
     // Get the input token size for a given sample index
     size_t getInputTokenSize(size_t sampleIdx) const {
-        if (allGenPhase) { return 1; }
+        if (allInputOne) { return 1; }
         return endTokenIdxs[sampleIdx] - (sampleIdx == 0 ? 0 : endTokenIdxs[sampleIdx - 1]);
+    }
+
+    // Get the input token offset inside the batch, for a given sample index
+    size_t getInputOffset(size_t sampleIdx) const {
+        return sampleIdx == 0 ? 0 : endTokenIdxs[sampleIdx - 1];
     }
 
     // Get the past token size for a given sample index
     size_t getPastTokenSize(size_t sampleIdx) const { return pastTokens[sampleIdx]; }
 
     // Get the total number of tokens inside the batch
-    size_t getTotalTokens() const {
-        return endTokenIdxs[batchSize - 1];
-    }
+    size_t getTotalTokens() const { return endTokenIdxs[batchSize - 1]; }
 
     // Get the sample index and token index inside the sample for a given token index
     // The input is not checked; the caller needs to ensure its correctness
     std::pair<size_t, size_t> getInfo(size_t tokenIdx) const {
         // If all samples are in the generation phase, the token index is the sample index
-        if (allGenPhase) { return {tokenIdx, 0}; }
+        if (allInputOne) { return {tokenIdx, 0}; }
 
         size_t sampleIdx = -1;
         for (size_t i = 0; i < batchSize; ++i) {
@@ -91,15 +104,23 @@ public:
                 break;
             }
         }
+
         if (sampleIdx >= 0) {
             size_t startIdx = sampleIdx == 0 ? 0 : endTokenIdxs[sampleIdx - 1];
             return {sampleIdx, tokenIdx - startIdx};
         }
+
+        printf("Error: cannot find the token index (%zu) inside the batch\n", tokenIdx);
         return {-1, -1};
     }
 
+    bool isPrefill() const { return bePrefill; }
+
 private:
     size_t batchSize;
+
+    // Input token size for each sample inside the batch
+    std::vector<size_t> inputTokens;
 
     // End token offset for each sample inside the batch
     std::vector<size_t> endTokenIdxs;
@@ -107,9 +128,12 @@ private:
     // Past token size for each sample inside the batch
     std::vector<size_t> pastTokens;
 
-    // Indicates whether all samples inside the batch are in the generation phase
     // This means that all samples have an input token size of 1
-    bool allGenPhase;
+    // Indicates whether all samples inside the batch are in the generation phase
+    bool allInputOne;
+
+    // Indicates whether the batch is in the prefill phase
+    bool bePrefill;
 };
 
 } // namespace xft
