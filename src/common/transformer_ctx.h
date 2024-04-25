8f351ba45061b27645e33bf7000e72a20a00a421
Changqing Li <changqing.li@intel.com>
2024-04-25 14:08:19 +0800

    [Common] Add sequenceMeta, sequenceGroup and sequenecePool. (#343)

--- src/common/transformer_ctx.h
+++ src/common/transformer_ctx.h
@@ -65,6 +65,10 @@ struct DecoderContext {
     // For custom usage
     int reserved1;
 
+#ifdef PIPELINE_PARALLEL
+    int sequenceID;
+#endif
+
     // Model structure configuration
     int vocabSize;
     int embeddingSize;
