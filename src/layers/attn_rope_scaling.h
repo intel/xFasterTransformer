27e6154aa6fffaf825251bf6b846e53cded03b6e
Meng,Chen <chen.meng@intel.com>
2024-04-26 08:54:51 +0800

    [Model] yarn-llama full-link bf16 support (#344)

--- src/layers/attn_rope_scaling.h
+++ src/layers/attn_rope_scaling.h
@@ -16,14 +16,23 @@
 #include <cmath>
 
 #include "attention.h"
+#include "decoder_layer.h"
 
-template <typename WeiT, typename QKPO_CLS, typename NORM_CLS>
-class RopeScalingAttention : public Attention<WeiT, QKPO_CLS, NORM_CLS, float, float, float> {
+template <typename WeiT, typename QKPO_CLS, typename NORM_CLS, typename InT = float, typename ImT = float,
+        typename OutT = float, bool INPUT_AS_RESID = true>
+class RopeScalingAttention : public Attention<WeiT, QKPO_CLS, NORM_CLS, InT, ImT, OutT, INPUT_AS_RESID> {
 public:
     RopeScalingAttention(int layerId, DecoderContext *ctx)
-        : Attention<WeiT, QKPO_CLS, NORM_CLS, float, float, float>(layerId, ctx) {
+        : Attention<WeiT, QKPO_CLS, NORM_CLS, InT, ImT, OutT, INPUT_AS_RESID>(layerId, ctx) {
         this->qkpo = QKPO_CLS(ctx->attHeadSize, ctx->maxPosEmbed, ctx->ropeParamsPtr);
     }
 
     virtual ~RopeScalingAttention() {}
+};
+
+template <typename WeiT, typename QKPO_CLS, typename NORM_CLS, typename InT, typename ImT, typename OutT>
+struct AttnTypeExtractor<RopeScalingAttention<WeiT, QKPO_CLS, NORM_CLS, InT, ImT, OutT, true>> {
+    using Tin = InT;
+    using Tim = ImT;
+    using Tout = OutT;
 };
