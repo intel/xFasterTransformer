2499f602c22184ca5afaa2f013ae0ff4e3bd4263
Duyi-Wang <duyi.wang@intel.com>
2024-04-30 15:30:59 +0800

    [Model] add interface for seq meta. (#366)

--- src/models/models.cpp
+++ src/models/models.cpp
@@ -31,9 +31,9 @@
 #include "qwen.h"
 #include "qwen2.h"
 #include "searcher.h"
+#include "sequence.h"
 #include "timeline.h"
 #include "yarn_llama.h"
-#include "sequence.h"
 
 namespace xft {
 enum class GenerationMode { GREEDY_SEARCH, BEAM_SEARCH, SAMPLE };
@@ -71,6 +71,7 @@ void Model::exitSlaves() {
     }
 }
 
+// TODO: deprecate the following function
 void Model::input(std::vector<int32_t> &inputIds_, int batchSize_) {
     isNewInput = true;
     Messenger &messenger = decoder->getMessenger();
@@ -88,6 +89,7 @@ void Model::input(std::vector<int32_t> &inputIds_, int batchSize_) {
     messenger.broadcast(inputIds.data(), dims[1]);
 }
 
+// TODO: deprecate the following function
 void Model::config(int maxLen_, int numBeams_, int numBeamHypsToKeep_, float lenPenalty_, bool doEarlyStopping_,
         int eosTokenId_, int padTokenId_, bool doSample_, float temperature_, int topK_, float topP_,
         float repetitionPenalty_, const std::vector<std::vector<int>> &stopWordsList_) {
@@ -107,6 +109,118 @@ void Model::config(int maxLen_, int numBeams_, int numBeamHypsToKeep_, float len
     this->config(configuration, stopWordsList_);
 }
 
+void syncStopWordsList(std::vector<std::vector<int>> &stopWordsList) {
+    Messenger &messenger = Messenger::getInstance();
+
+    int listSize = stopWordsList.size();
+    messenger.broadcast(&listSize, 1);
+    // If stopWordsList is empty, stop broadcasting and return.
+    if (listSize == 0) { return; }
+
+    vector<int> wordsSize(listSize);
+    if (messenger.getRank() == 0) {
+        for (int i = 0; i < listSize; i++) {
+            wordsSize[i] = stopWordsList[i].size();
+        }
+    }
+    messenger.broadcast(wordsSize.data(), listSize);
+
+    int wordsDataLen = 0;
+    for (auto x : wordsSize) {
+        wordsDataLen += x;
+    }
+
+    // flatten to 1-D vector
+    vector<int> wordsData(wordsDataLen);
+    if (messenger.getRank() == 0) {
+        int currentIndex = 0;
+        for (const auto &words : stopWordsList) {
+            std::copy(words.begin(), words.end(), wordsData.begin() + currentIndex);
+            currentIndex += words.size();
+        }
+    }
+    messenger.broadcast(wordsData.data(), wordsDataLen);
+
+    if (messenger.getRank() != 0) {
+        // restore stop words list to 2-D vector
+        std::vector<std::vector<int>> restoredList;
+        int currentIndex = 0;
+        for (int i = 0; i < wordsSize.size(); ++i) {
+            int size = wordsSize[i];
+            std::vector<int> subVector(wordsData.begin() + currentIndex, wordsData.begin() + currentIndex + size);
+            currentIndex += size;
+            restoredList.emplace_back(subVector);
+        }
+    }
+}
+
+void Model::set_input(std::vector<int32_t> &inputIds_, int batchSize_, int maxLen_, int numBeams_,
+        int numBeamHypsToKeep_, float lenPenalty_, bool doEarlyStopping_, int eosTokenId_, int padTokenId_,
+        bool doSample_, float temperature_, int topK_, float topP_, float repetitionPenalty_,
+        const std::vector<std::vector<int>> &stopWordsList_) {
+    configuration.maxLen = maxLen_;
+    configuration.numBeams = numBeams_;
+    configuration.numBeamHypsToKeep = numBeamHypsToKeep_;
+    configuration.lenPenalty = lenPenalty_;
+    configuration.doEarlyStopping = doEarlyStopping_;
+    configuration.eosTokenId = eosTokenId_;
+    configuration.padTokenId = padTokenId_;
+    configuration.doSample = doSample_;
+    configuration.temperature = temperature_;
+    configuration.topK = topK_;
+    configuration.topP = topP_;
+    configuration.repetitionPenalty = repetitionPenalty_;
+
+    this->set_input(inputIds_, batchSize_, configuration, stopWordsList_);
+}
+
+void Model::set_input(std::vector<int32_t> &inputIds_, int batchSize_, SearcherConfig &config_,
+        const std::vector<std::vector<int>> &stopWordsList_) {
+    // TODO: remove new_input flag
+    if (config_.eosTokenId == -1) { config_.eosTokenId = decoder->getEndId(); }
+    if (config_.padTokenId == -1) { config_.padTokenId = config_.eosTokenId; }
+    SamplingMeta samplingMeta(config_, stopWordsList_);
+
+    Messenger &messenger = Messenger::getInstance();
+    if (isMaster()) { inputIds = inputIds_; }
+
+    // Sync input and sampling param in distributed mode.
+    if (messenger.getSize() > 1) {
+        // [batch size, inputIds size]
+        int dims[2];
+        if (isMaster()) {
+            dims[0] = batchSize_;
+            dims[1] = inputIds_.size();
+        }
+        messenger.broadcast(dims, 2);
+        batchSize = dims[0];
+        seqLen = dims[1] / batchSize;
+
+        inputIds.resize(dims[1]);
+        messenger.broadcast(inputIds.data(), dims[1]);
+
+        messenger.broadcast((int *)&samplingMeta.config, sizeof(SearcherConfig) / sizeof(int));
+
+        syncStopWordsList(samplingMeta.stopWordsList);
+    } else {
+        batchSize = batchSize_;
+        seqLen = inputIds_.size() / batchSize_;
+    }
+
+    SequencePool &seqPool = SequencePool::getInstance();
+    InputQueue &inputQueue = InputQueue::getInstance();
+    for (int i = 0; i < batchSize; i++) {
+        auto group = seqPool.newGroupMeta(inputIds, samplingMeta);
+        inputQueue.push(group);
+    }
+
+    xft::workingGroup.clear();
+    while (!inputQueue.empty()) {
+        xft::workingGroup.push_back(inputQueue.pop());
+    }
+}
+
+// TODO: Deprecate the following function
 void Model::config(SearcherConfig &config_, const std::vector<std::vector<int>> &stopWordsList_) {
     isNewInput = true;
     if (decoder->getRank() == 0) { configuration = config_; }
@@ -121,33 +235,63 @@ void Model::config(SearcherConfig &config_, const std::vector<std::vector<int>> 
 }
 
 bool Model::isDone() {
-    if (searcher == nullptr || inputIds.empty()) {
-        printf("Please set input and config first.\n");
-        exit(-1);
+    // TODO: Deprecate the following Path
+    if (searcher != nullptr) {
+        if (inputIds.empty()) {
+            printf("Please set input and config first.\n");
+            exit(-1);
+        }
+        return !isNewInput && searcher->isDone();
     }
-    return !isNewInput && searcher->isDone();
+    for (auto x : xft::workingGroup) {
+        if (!x->isDone()) { return false; }
+    }
+    return true;
 }
 
-std::tuple<float *, int, int> Model::forward() {
-    int64_t dims[3] = {batchSize, 1, seqLen};
-    return decoder->forward(inputIds.data(), dims, 0, true);
+std::tuple<float *, int, int> Model::forward(bool logits_all) {
+    // TODO: Deprecate the following Path
+    if (searcher != nullptr) {
+        int64_t dims[3] = {batchSize, 1, seqLen};
+        return decoder->forward(inputIds.data(), dims, 0, logits_all);
+    }
+    // TODO: checking waiting queue
+    if (workingGroup.empty()) {
+        printf("Please input prompt first.\n");
+        exit(-1);
+    }
+    // Assume that all sequences in the group are all prompts or all decodes.
+    // Prepare input data for the decoder.
+    std::vector<SequenceMeta *> workingSeqs;
+    for (auto x : workingGroup) {
+        workingSeqs.push_back(x->get(0));
+        if (x->getGroupSize() > 1 && x->getStep() > 1) {
+            for (int32_t i = 1; i < x->getGroupSize(); i++) {
+                workingSeqs.push_back(x->get(i));
+            }
+        }
+    }
+
+    return decoder->forward(workingSeqs, logits_all);
 }
 
 std::vector<int32_t> Model::generate() {
-    if (inputIds.empty()) {
-        printf("Please set input tokens by model.input().\n");
-        exit(-1);
-    }
-    if (searcher == nullptr) {
-        printf("Please set generation config by model.config().\n");
-        exit(-1);
-    }
-
-    if (isNewInput) {
-        isNewInput = false;
-        return searcher->getNextToken(inputIds.data(), batchSize, inputIds.size() / batchSize);
+    // TODO: Deprecate the following Path
+    if (searcher != nullptr) {
+        if (inputIds.empty()) {
+            printf("Please set input tokens by model.input().\n");
+            exit(-1);
+        }
+        if (isNewInput) {
+            isNewInput = false;
+            return searcher->getNextToken(inputIds.data(), batchSize, inputIds.size() / batchSize);
+        } else {
+            return searcher->getNextToken();
+        }
     } else {
-        return searcher->getNextToken();
+        // TODO
+        throw std::logic_error("Method not implemented");
+        return {};
     }
 }
 
