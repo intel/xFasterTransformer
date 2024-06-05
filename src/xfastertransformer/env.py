a2d33c6d9fc52a9ab586a2e7ec8eeccb565c2af1
Duyi-Wang <duyi.wang@intel.com>
2024-06-05 12:41:37 +0800

    [Dependency] Fix wrong so path returned in `get_env()`. (#432)

--- src/xfastertransformer/env.py
+++ src/xfastertransformer/env.py
@@ -14,6 +14,8 @@
 # ============================================================================
 import os
 def get_env():
-    libiomp_path = os.path.dirname(os.path.abspath(__file__)) + "/libxft_comm_helper.so"
-    env = f"LD_PRELOAD={libiomp_path}:$LD_PRELOAD"
+    libiomp_path = os.path.dirname(os.path.abspath(__file__)) + "/libiomp5.so"
+    env = f"LD_PRELOAD={libiomp_path}"
+    if os.getenv("LD_PRELOAD"):
+        env = f"{env}:{os.getenv('LD_PRELOAD')}"
     return env
