862a7ceee1c0fd72e8a3aa214e0c3954c557d0da
Duyi-Wang <duyi.wang@intel.com>
2025-03-19 09:40:44 +0800

    [CMake] Check OneDNN version. (#106)

--- cmake/onednn.cmake
+++ cmake/onednn.cmake
@@ -31,11 +31,41 @@ endif()
 
 set(ONEDNN_3rdparty_DIR "${CMAKE_SOURCE_DIR}/3rdparty/onednn")
 
-if(NOT EXISTS ${ONEDNN_3rdparty_DIR})
+set(ONEDNN_TAG "v3.5")
+
+set(SKIP_FLAG FALSE)
+
+if(EXISTS ${ONEDNN_3rdparty_DIR})
+    file(GLOB FILES "${ONEDNN_3rdparty_DIR}/*")
+
+    if(FILES)
+        find_program(GIT_EXECUTABLE git)
+        if(NOT GIT_EXECUTABLE)
+            message(FATAL_ERROR "Git not found. Aborting.")
+        endif()
+
+        execute_process(
+        COMMAND ${GIT_EXECUTABLE} describe --tags
+        WORKING_DIRECTORY ${ONEDNN_3rdparty_DIR}
+        OUTPUT_VARIABLE CURRENT_TAG
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+
+        message(STATUS "Current TAG of OneDNN : ${CURRENT_TAG}")
+
+        if(CURRENT_TAG STREQUAL ${ONEDNN_TAG})
+            set(SKIP_FLAG TRUE)
+        endif()
+    else()
+        file(REMOVE_RECURSE ${ONEDNN_3rdparty_DIR})
+    endif()
+endif()
+
+if(NOT SKIP_FLAG)
     # cmake-format: off
     ExternalProject_Add(onednn
       GIT_REPOSITORY    https://github.com/oneapi-src/oneDNN.git
-      GIT_TAG           v3.5
+      GIT_TAG           ${ONEDNN_TAG}
       SOURCE_DIR        ${ONEDNN_3rdparty_DIR}
       BINARY_DIR        ${ONEDNN_3rdparty_DIR}
       CONFIGURE_COMMAND ${CMAKE_COMMAND} -E make_directory "build" && ${CMAKE_COMMAND} -E chdir "build" ${CMAKE_COMMAND} ${ONEDNN_BUILD_OPTIONS} ..
